7d41e05fc72562b04ec86e881821b3b2ab9cb417
theskytalos <junior.venncio@gmail.com>
2020-07-17 13:57:51 -0300

    Older API removed

--- chefia-api/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    try {
-        $pdo = new PDO("mysql:host=127.0.0.1;dbname=chefia_db;", "db-user", "123");
-    } catch (PDOException $dbError) {
-        echo "Ocorreu um problema com o banco de dados.\n\n" . $dbError;
-        exit();
-    }
-// Little test
-?>
